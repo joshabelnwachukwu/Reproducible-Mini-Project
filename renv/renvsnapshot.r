11bccfbcd5d7d13634fc3665c9217d7fa6473653
Joshua U Abel-Nwachukwu <joshabel2002@gmail.com>
2025-09-29 23:46:03 -0700

    edited lock file/packages

--- renv/renvsnapshot.r
+++ renv/renvsnapshot.r
@@ -1,2 +1,15 @@
 #Snapshot of in-use packages 
+#Snapshot of in-use packages 
+install.packages("palmerpenguins")
+install.packages("here")
+install.packages("tidyverse")
+install.packages("janitor")
+install.packages("broom")
+library(palmerpenguins)
+library(here)
+library(tidyverse)
+library(janitor)
+library(broom)
+
 renv::snapshot()
+renv::snapshot(packages = c("palmerpenguins", "here", "tidyverse", "janitor", "broom"))
